4966889b4d7cfcecb0575acbd213014ef9aae6af
helloooooo <yomicerisier@gmail.com>
2018-07-22 19:55:18 +0900

    update

---
+++ src/abc064a.rs
@@ -0,0 +1,24 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let v = read_vec::<u64>();
+    if (v[0]*100 + v[1]*10 +v[2]) % 4 == 0{
+        println!("YES");
+    } else{
+        println!("NO");
+    }
+}
---
+++ src/abc064b.rs
@@ -0,0 +1,23 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let n : u64 = read();
+    let mut v = read_vec::<u64>();
+    v.sort();
+    let ans = v.last().unwrap() -v[0];
+    println!("{}",ans );
+}
