e5a21de68b715c519b0c843d676f24a82b032c86
helloooooo <yomicerisier@gmail.com>
2018-08-05 22:52:47 +0900

    update

---
+++ src/abc104b.rs
@@ -0,0 +1,30 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let s:String = read();
+    let len = s.chars().count();
+    if s.chars().skip(2).take(len-3).filter(|&x| x=='C').count() == 1 {
+        if s.chars().nth(0).unwrap()== 'A'{
+            if s.chars().filter(|&x| x.is_lowercase()).count() == (len-2){
+                println!("AC");
+                return;
+            }
+        }
+    }
+    println!("WA");
+}
+//s.chars().skip(2).take(len - 1).filter(|&x| x == 'C').count() == 1
---
+++ src/abc104c.rs
@@ -0,0 +1,53 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let dg = read_vec::<i64>();
+    let (d,g) = (dg[0],dg[1]);
+    let mut score :Vec<Vec<i64>> = Vec::new();
+    let mut pcn = read_vec2::<i64>(d as u32);
+    let mut ans :Vec<i64> = Vec::new();
+    for j in 0..d {
+        score.push(Vec::new());
+        for k in 1..pcn[j as usize][0]+1{
+                if pcn[j as usize][0] == k{
+                    score[j as usize].push(k*(j+1)*100+pcn[j as usize][1]);
+                } else{
+                     score[j as usize].push(k*(j+1)*100);
+                }
+        }
+    }
+
+    println!("{:?}",score );
+    score.reverse();
+    for j in 0..d{
+        
+        let mut count = 0;
+        'outer: for k in 0..j {
+            for l in 0..pcn[j as usize][0]{
+                sub = sub -score[k as usize][l as usize];
+                count += 1;
+                if sub <= 0{
+                    break 'outer;
+                }
+            }
+        }
+        if sub <= 0{
+            ans.push(count);
+        }
+    }
+    println!("{:?}",ans.iter().min().unwrap() );
+}
