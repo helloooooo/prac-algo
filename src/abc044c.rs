515316753d05a90bfd1c53f00762f90eb4bc93c0
helloooooo <yomicerisier@gmail.com>
2018-08-20 22:09:29 +0900

    update

--- src/abc044c.rs
+++ src/abc044c.rs
@@ -15,5 +15,33 @@ fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
     (0..n).map(|_| read_vec()).collect()
 }
 fn main(){
-    
+    let mut n : i64 = read();
+    let mut ln  = read_vec::<i64>();
+    // ln.sort();
+    let mut ans = 0;
+    while n > 1 {
+        let mut (mii1,mii2) = (0,1);
+        if ln[mii1 as usize] > ln[mii2 as usize] {
+            let tmp = mii1;
+            mii1 = mii2;
+            mii2 = tmp;
+        }
+        for i in 2..n {
+            if ln[i] < ln[mii2] {
+                mii2 = i;
+            }
+        }
+
+        let t = ln[mii1] + ln[mii2];
+        ans += t;
+        if mii1 = n-1 {
+            let tmp = mii1;
+            mii1 = mii2;
+            mii2 = tmp;
+        }
+        ln[mii1] = t;
+        ln[mii2] = ln[n-1];
+        n -= 1;
+    }
+    println!("{}",ans);
 }
