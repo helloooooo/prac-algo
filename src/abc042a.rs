7c1a4a101bb14e7adcc5cc91bd84fa6e538cddfd
helloooooo <yomicerisier@gmail.com>
2018-08-22 22:15:31 +0900

    update

---
+++ src/abc042a.rs
@@ -0,0 +1,24 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let s = read_vec::<i64>();
+    let ans1 = s.iter().filter(|&x| *x == 7).count() == 1;
+    let ans2 = s.iter().filter(|&x| *x == 5).count() == 2;
+    println!("{}",if ans1 && ans2  {
+        "YES"
+    } else { "NO"});
+}
