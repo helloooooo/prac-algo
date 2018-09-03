30229e4134f647ca25dbe0f03d7d65ede8d284e8
helloooooo <yomicerisier@gmail.com>
2018-09-03 22:04:06 +0900

    update

---
+++ src/abc034a.rs
@@ -0,0 +1,25 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let  xy = read_vec::<i64>();
+    let ans = if xy[0] < xy[1] {
+        "Better"
+    } else {
+        "Worse"
+    };
+    println!("{}",ans);
+}
