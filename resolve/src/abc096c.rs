286ac92433f766462a909f74ec035129f7ee5264
helloooooo <yomicerisier@gmail.com>
2018-09-13 00:25:30 +0900

    update

--- resolve/src/abc096c.rs
+++ resolve/src/abc096c.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-use std::iter::FromIterator;
 fn read<T: std::str::FromStr>() -> T {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
@@ -17,20 +15,30 @@ fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
     (0..n).map(|_| read_vec()).collect()
 }
 fn main(){
-    let s:String = read();
-    let n:usize = read();
-    let s = s.chars().collect::<Vec<char>>();
-    let mut set = HashSet::new();
-    for j in 1..s.len()+1{
-        let v:Vec<String> = s.windows(j)
-        .map(|x| x.iter().map(|c| *c).collect())
-        .collect();
-        for k in v {
-            set.insert(k);
+    let (h,w) = {
+        let t = read_vec::<i64>();
+        (t[0],t[1])
+    };
+    let mut field:Vec<Vec<char>> = vec![];
+    for _ in 0..h {
+        field.push(read::<String>().chars().collect());
+    }
+    for j in 0..h {
+        for k in 0..w {
+            if 0 <= j - 1 && j + 1  < h &&  0 <= k-1 && k+1 < w{
+                let mut v = vec![];
+                v.push(field[(j-1) as usize][k as usize]);
+                v.push(field[(j+1) as usize][k as usize]);
+                v.push(field[j as usize][(k-1) as usize]);
+                v.push(field[j as usize][(k+1) as usize]);
+                if field[j as usize][k as usize] == '#'{
+                    if v.into_iter().filter(|&x| x == '.').count() == 4 {
+                        println!("No");
+                        return;
+                    }
+                }
+            } 
         }
     }
-    let mut ans:Vec<String> = set.into_iter()
-        .collect();
-    ans.sort();
-    println!("{}",ans[n-1] );
+    println!("Yes");
 }
