5ffc91e379a3eeae58632524cb5482d84ad2e42d
helloooooo <yomicerisier@gmail.com>
2018-09-25 23:45:54 +0900

    update

--- src/abc110c.rs
+++ src/abc110c.rs
@@ -3,14 +3,14 @@ fn read<T: std::str::FromStr>() -> T {
     std::io::stdin().read_line(&mut s).ok();
     s.trim().parse().ok().unwrap()
 }
- 
+
 fn read_vec<T: std::str::FromStr>() -> Vec<T> {
     read::<String>()
         .split_whitespace()
         .map(|e| e.parse().ok().unwrap())
         .collect()
 }
- 
+
 fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
     (0..n).map(|_| read_vec()).collect()
 }
