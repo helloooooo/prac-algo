96e61589a10adac8602658fc9a178532a5cae82f
helloooooo <yomicerisier@gmail.com>
2018-10-03 23:57:03 +0900

    update

---
+++ resolve/src/abc074c.rs
@@ -0,0 +1,23 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let (a,b,c,d,e,f) = {
+        let t = read_vec::<usize>();
+        (t[0],t[1],t[2],t[3],t[4],t[5])
+    };
+    
+}
