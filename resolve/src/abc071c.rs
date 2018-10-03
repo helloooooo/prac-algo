408d42fe1264bf577775b2d45a9c7326d1839e4c
helloooooo <yomicerisier@gmail.com>
2018-10-04 00:35:09 +0900

    update

--- resolve/src/abc071c.rs
+++ resolve/src/abc071c.rs
@@ -18,7 +18,22 @@ fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
 fn main(){
     let n:i64 = read();
     let an = read_vec::<i64>();
-    let ans = an.into_iter().fold(HashMap::new(),|mut map ,x|{
-        
-    })
+    let mut ans:Vec<(_,_)> = an.into_iter().fold(HashMap::new(),|mut map ,x|{
+        let y = map.get(&x).map(|i| i+1).unwrap_or(1);
+        map.insert(x,y);
+        map
+    }).into_iter().collect::<Vec<(_,_)>>()
+    .into_iter()
+    .filter(|x| x.1 >= 2)
+    .collect();
+    if ans.len() < 2 {
+        println!("0");
+        return;
+    }
+    ans.sort_by_key(|x| x.0);
+    if ans.iter().enumerate().any(|(i,x)| x.1 >= 4 && i == ans.len()-1 ){
+        println!("{}",ans.last().unwrap().0 * ans.last().unwrap().0);
+        return;
+    }
+    println!("{}",ans.last().unwrap().0 * ans[ans.len()-2].0);
 }
