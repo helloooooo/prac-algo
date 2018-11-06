bc15cefe2e2663c217677a0c86944042718f90da
NaokiSatou <yomicerisier@gmail.com>
2018-11-07 00:33:51 +0900

    update

--- src/abc113c.rs
+++ src/abc113c.rs
@@ -21,11 +21,13 @@ fn main(){
     };
     let pmym = read_vec2::<i64>(m as u32);
     let mut test = vec!["".to_string();m as usize];
-    let mut sub:Vec<_> = pmym.clone().into_iter().enumerate().collect();
-    sub.sort_by_key(|k| k.1[1]);
+    let mut sub:Vec<_> = pmym.clone().into_iter().enumerate().map(|x|(x.1,x.0)).collect();
+    // sub.sort_by_key(|k| k.1[1]);
+    sub.sort();
+    // println!("{:?}",sub);
     let mut b_key = 0;
     let mut c = 0;
-    for (k,v) in sub{
+    for (v,k) in sub{
         if b_key != v[0] {
             c = 1;
         }else {
