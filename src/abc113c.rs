d4180eabbb2b6f1ddb32eb36dcf1380218668469
NaokiSatou <yomicerisier@gmail.com>
2018-11-08 00:18:08 +0900

    training

--- src/abc113c.rs
+++ src/abc113c.rs
@@ -1,4 +1,3 @@
-
 fn read<T: std::str::FromStr>() -> T {
     let mut s = String::new();
     std::io::stdin().read_line(&mut s).ok();
@@ -14,29 +13,34 @@ fn read_vec<T: std::str::FromStr>() -> Vec<T> {
 fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
     (0..n).map(|_| read_vec()).collect()
 }
-fn main(){
-    let (n,m) = {
+fn main() {
+    let (n, m) = {
         let t = read_vec::<i64>();
-        (t[0],t[1])
+        (t[0], t[1])
     };
     let pmym = read_vec2::<i64>(m as u32);
-    let mut test = vec!["".to_string();m as usize];
-    let mut sub:Vec<_> = pmym.clone().into_iter().enumerate().map(|x|(x.1,x.0)).collect();
+    let mut test = vec!["".to_string(); m as usize];
+    let mut sub: Vec<_> = pmym
+        .clone()
+        .into_iter()
+        .enumerate()
+        .map(|x| (x.1, x.0))
+        .collect();
     // sub.sort_by_key(|k| k.1[1]);
     sub.sort();
     // println!("{:?}",sub);
     let mut b_key = 0;
     let mut c = 0;
-    for (v,k) in sub{
+    for (v, k) in sub {
         if b_key != v[0] {
             c = 1;
-        }else {
+        } else {
             c += 1;
         }
-        test[k] = format!("{:>06}{:>06}",v[0],c);
+        test[k] = format!("{:>06}{:>06}", v[0], c);
         b_key = v[0]
     }
-    for v in test{
-        println!("{}",v);
+    for v in test {
+        println!("{}", v);
     }
 }
