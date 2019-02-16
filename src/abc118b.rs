ae52b775882a0b791ed3c0b57599916d59fe3529
helloooooo <yomicerisier@gmail.com>
2019-02-17 00:19:37 +0900

    add

---
+++ src/abc118b.rs
@@ -0,0 +1,34 @@
+use std::collections::HashMap;
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let (n,m) ={
+        let t = read_vec::<usize>();
+        (t[0],t[1])
+    };
+    let v = read_vec2::<usize>(n as u32);
+    let mut map = HashMap::new();
+    for j in 0..n {
+        for k in 1..v[j].len(){
+            let y = map.get(&v[j][k]).map(|i| i + 1).unwrap_or(1);
+            map.insert(v[j][k],y);
+        }
+    }
+    let ans = map.into_iter().filter(|&(k,value)|{
+        value == n
+    }).count();
+    
+    println!("{}",ans);
+}
---
+++ src/abc118c.rs
@@ -0,0 +1,29 @@
+fn read<T: std::str::FromStr>() -> T {
+    let mut s = String::new();
+    std::io::stdin().read_line(&mut s).ok();
+    s.trim().parse().ok().unwrap()
+}
+fn read_vec<T: std::str::FromStr>() -> Vec<T> {
+    read::<String>()
+        .split_whitespace()
+        .map(|e| e.parse().ok().unwrap())
+        .collect()
+}
+fn read_vec2<T: std::str::FromStr>(n: u32) -> Vec<Vec<T>> {
+    (0..n).map(|_| read_vec()).collect()
+}
+fn main(){
+    let n = read::<usize>();
+    let an = read_vec::<i64>();
+    let ans = an.iter().fold(0,|acc,&x| {
+        gcd(acc,x)
+    });
+    println!("{}",ans);
+}
+
+fn gcd(x:i64,y:i64) -> i64 {
+    if y == 0 { x
+    } else {
+        gcd(y,x % y)
+    }
+}
