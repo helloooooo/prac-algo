d22f4964e58c8b26aff435cfc7ddedca4c25aa51
helloooooo <yomicerisier@gmail.com>
2019-04-03 23:43:37 +0900

    update

---
+++ resolve/src/abc122c.rs
@@ -0,0 +1,71 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+fn main(){
+    input!{
+        n:i64,
+        q:i64,
+        s:chars,
+        lrn:[(usize,usize);q]
+    }
+    let mut count = 0;
+    let mut sum:Vec<i64> = vec![0];
+    for j in 0..(n as usize-1) {
+        if s[j] == 'A' && s[j+1] == 'C' {
+            count += 1;
+        }
+        sum.push(count)
+    }
+    for &(start,end) in &lrn {
+        println!("{}",sum[end-1]-sum[start-1]);
+    }
+}
