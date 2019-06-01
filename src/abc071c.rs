f4c345e066907cba755a70ff157a096488c3754e
helloooooo <yomicerisier@gmail.com>
2019-06-01 20:54:59 +0900

    update

---
+++ src/abc071c.rs
@@ -0,0 +1,84 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+use std::collections::HashMap;
+fn main(){
+    input!{
+        n:usize,
+        an:[i64;n],
+    }
+    let mut target:Vec<_> = an.iter().fold(HashMap::new(),|mut map:HashMap<_,_>,x| {
+        let y = map.get(&x).map(|&i| i+1).unwrap_or(1);
+        map.insert(x,y);
+        map
+    })
+    .into_iter()
+    .flat_map(|(k,v)| {
+        if v >= 4 {
+            vec![k,k]
+        } else if v >= 2 {
+            vec![k]
+        } else {
+            vec![]
+        }
+    })
+    .collect();
+    target.sort();
+    target.reverse();
+    let ans = if target.len() >= 2{ target[0] * target[1] } else { 0 };
+    println!("{}",ans);
+}
