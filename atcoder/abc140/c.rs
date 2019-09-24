513ee868b84cd0e3f8fafc76bc22372b4a878c18
helloooooo <yomicerisier@gmail.com>
2019-09-25 00:00:56 +0900

    140

---
+++ atcoder/abc140/c.rs
@@ -0,0 +1,91 @@
+macro_rules! input {
+    (source = $s:expr, $($r:tt)*) => {
+        let mut iter = $s.split_whitespace();
+        let mut next = || { iter.next().unwrap() };
+        input_inner!{next, $($r)*}
+    };
+    ($($r:tt)*) => {
+        let stdin = std::io::stdin();
+        let mut bytes = std::io::Read::bytes(std::io::BufReader::new(stdin.lock()));
+        let mut next = move || -> String{
+            bytes
+                .by_ref()
+                .map(|r|r.unwrap() as char)
+                .skip_while(|c|c.is_whitespace())
+                .take_while(|c|!c.is_whitespace())
+                .collect()
+        };
+        input_inner!{next, $($r)*}
+    };
+}
+
+macro_rules! input_inner {
+    ($next:expr) => {};
+    ($next:expr, ) => {};
+
+    ($next:expr, $var:ident : $t:tt $($r:tt)*) => {
+        let $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+    ($next:expr, mut $var:ident : $t:tt $($r:tt)*) => {
+        let mut $var = read_value!($next, $t);
+        input_inner!{$next $($r)*}
+    };
+}
+
+macro_rules! read_value {
+    ($next:expr, ( $($t:tt),* )) => {
+        ( $(read_value!($next, $t)),* )
+    };
+
+    ($next:expr, [ $t:tt ; $len:expr ]) => {
+        (0..$len).map(|_| read_value!($next, $t)).collect::<Vec<_>>()
+    };
+
+    ($next:expr, chars) => {
+        read_value!($next, String).chars().collect::<Vec<char>>()
+    };
+
+    ($next:expr, usize1) => {
+        read_value!($next, usize) - 1
+    };
+
+    ($next:expr, $t:ty) => {
+        $next().parse::<$t>().expect("Parse error")
+    };
+}
+use std::collections::HashMap;
+use std::cmp::{max,min};
+fn main(){
+    input!{
+        n:i64,
+        k:i64,
+        q:i64,
+        aq:[i64;q],
+    }
+    let mut map = HashMap::new();
+    for &a in &aq {
+        *map.entry(a).or_insert(0) += 1;
+    }
+    for i in 1..n+1 {
+        let ans = match map.get(&i) {
+            Some(v) => if (k+v-q) <= 0 {"No"} else {"Yes"},
+            None => if (k-q) <= 0 {"No"} else {"Yes"},
+        };
+        println!("{}",ans);
+    }
+}
+
+
+fn is_prime(x:i64) -> bool {
+    if x == 2 {return true;}
+    if x < 2 || x % 2 == 0 {return false;}
+    let mut j = 3;
+    while j <= (x as f64).sqrt() as i64 {
+        if x % j == 0 {
+            return false;
+        }
+        j += 2;
+    }
+    true
+}
